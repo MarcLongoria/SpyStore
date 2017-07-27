d19642fda4bc1e1a4c610ce5325f9c89b1bc30a3
Marc Longoria <marclongoria@outlook.com>
2017-07-27 16:50:20 -0500

    Adding Entities and EF

---
+++ SpyStore.DAL/SpyStore.DAL/EF/MyConnectionStrategy.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace SpyStore.DAL.EF
+{
+    public class MyConnectionStrategy : ExecutionStrategy
+    {
+        public MyConnectionStrategy(ExecutionStrategyContext context) : base(context, ExecutionStrategy.DefaultMaxRetryCount, ExecutionStrategy.DefaultMaxDelay)
+        {
+
+        }
+
+        public MyConnectionStrategy(ExecutionStrategyContext context, int maxRetryCount, TimeSpan maxRetryDelay) : base(context, maxRetryCount, maxRetryDelay)
+        {
+
+        }
+
+        protected override bool ShouldRetryOn(Exception exception)
+        {
+            return true;
+        }
+    }
+}
---
+++ SpyStore.DAL/SpyStore.Models/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using SpyStore.Models.Entities.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpyStore.Models.Entities
+{
+    [Table("Categories", Schema = "Store")]
+    public class Category : EntityBase
+    {
+        [DataType(DataType.Text), MaxLength(50)]
+        public string CategoryName { get; set; }
+    }
+}
