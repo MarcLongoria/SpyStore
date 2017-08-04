c174a0e43bc4958bcc290feb96b332a03dacb618
Marc Longoria <marclongoria@outlook.com>
2017-08-04 16:25:34 -0500

    Commit Category test changes.

---
+++ SpyStore.DAL/SpyStore.DAL.Tests/ContextTests/CategoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SpyStore.DAL.EF;
+using SpyStore.Models.Entities;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+namespace SpyStore.DAL.Tests.ContextTests
+{
+    [Collection("SpyStore.DAL")]
+    public class CategoryTests : IDisposable    
+    {
+        private readonly StoreContext _db;
+        public CategoryTests()
+        {
+            _db = new StoreContext();
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+    }
+}
